3ac2e17760acb433a1c1dd72e15caa498a68a1a1
Dajve Green <david.green@klevu.com>
2022-03-30 01:01:12 +0100

    This is a sub package to the klevu-smart-search-M2 repository.
    It is for allowing the CMS content to be indexed with Klevu Search.
    
    Please, see klevu-smart-search-M2/README.md for more information on
    how to install the overall extension.

--- Model/LoadAttribute.php
+++ Model/LoadAttribute.php
@@ -56,9 +56,9 @@ class LoadAttribute extends  AbstractModel
             $value["id"] = "pageid_" . $value["page_id"];
             $value["url"] = $base_url . $value["identifier"];
             $desc = preg_replace("/<script\b[^>]*>(.*?)<\/script>/is", "", html_entity_decode($value["content"]));
-            $value["desc"] = preg_replace('#\{{.*?\}}#s', '', strip_tags($this->_contentHelperData->ripTags($desc)));
+            $value["desc"] = preg_replace('#\{{.*?\}}#s', '', strip_tags((string)$this->_contentHelperData->ripTags($desc)));
             $value["metaDesc"] = $value["meta_description"] . $value["meta_keywords"];
-            $value["shortDesc"] = substr(preg_replace('#\{{.*?\}}#s', '', strip_tags($this->_contentHelperData->ripTags($desc))), 0, 200);
+            $value["shortDesc"] = substr(preg_replace('#\{{.*?\}}#s', '', strip_tags((string)$this->_contentHelperData->ripTags($desc))), 0, 200);
             $value["listCategory"] = "KLEVU_CMS";
             $value["category"] = "pages";
             $value["salePrice"] = 0;
