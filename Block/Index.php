548a8889b450c49d5258da73666ce1a44f3b5428
Richard Hardman <richard.hardman@klevu.com>
2023-04-27 09:12:08 +0100

    Release Version 3.1.0

--- Block/Index.php
+++ Block/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Klevu\Content\Block;
 
 use Magento\Framework\View\Element\Template;
@@ -9,120 +10,131 @@ use Klevu\Search\Helper\Config as Klevu_SearchHelperConfig;
 
 class Index extends Template
 {
-	protected $_context;
-	
-	/**
-     * @param Context $context     
-	 * @param Klevu_ContentHelper $contentHelperData
+    /**
+     * @var Context
+     */
+    protected $_context;
+    /**
+     * @var Klevu_ContentHelper
+     */
+    protected $_contentHelperData;
+    /**
+     * @var Klevu_SearchHelperConfig
+     */
+    protected $_searchHelperConfig;
+    /**
+     * @var Magento_CatalogSearchHelper
+     */
+    protected $_catalogSearchHelper;
+
+    /**
+     * @param Context $context
+     * @param Klevu_ContentHelper $contentHelperData
      * @param Klevu_SearchHelperConfig $searchHelperConfig
-	 * @param Magento_CatalogSearchHelper $catalogSearchHelper
+     * @param Magento_CatalogSearchHelper $catalogSearchHelper
      * @param array $data
      */
-	public function __construct(
-		Context $context,             
-        Klevu_ContentHelper $contentHelperData,     
-		Klevu_SearchHelperConfig $searchHelperConfig,       
-		Magento_CatalogSearchHelper $catalogSearchHelper,
-		array $data = []
-    
-	) {
-		$this->_context = $context;
-		parent::__construct($context, $data);        
-        $this->_contentHelperData = $contentHelperData;    
-		$this->_searchHelperConfig = $searchHelperConfig;		        
-		$this->_catalogSearchHelper = $catalogSearchHelper;
+    public function __construct(
+        Context $context,
+        Klevu_ContentHelper $contentHelperData,
+        Klevu_SearchHelperConfig $searchHelperConfig,
+        Magento_CatalogSearchHelper $catalogSearchHelper,
+        array $data = []
+    ) {
+        $this->_context = $context;
+        parent::__construct($context, $data);
+        $this->_contentHelperData = $contentHelperData;
+        $this->_searchHelperConfig = $searchHelperConfig;
+        $this->_catalogSearchHelper = $catalogSearchHelper;
     }
-	
-    
+
     /**
      * Get the Klevu other content
-	 *
+     *
      * @return array
      */
     public function getCmsContent()
-    {		
+    {
         return $this->_contentHelperData->getCmsData();
-         
     }
-	
+
     /**
      * Return the Klevu other content filters
-	 *
+     *
      * @return array
      */
     public function getContentFilters()
     {
-        return $this->_contentHelperData->getKlevuFilters();      
+        return $this->_contentHelperData->getKlevuFilters();
     }
-	
-	/**
+
+    /**
      * Return query param from request
-	 *
+     *
      * @return string
      */
-	public function getQueryParam() 
-	{		
-		return $this->_context->getRequest()->getParam("q");
-	}
-	
-	/**
+    public function getQueryParam()
+    {
+        return $this->_context->getRequest()->getParam("q");
+    }
+
+    /**
      * Check whether the cms enabled or not
-	 *
+     *
      * @return bool
      */
-	public function isCmsSyncEnabledOnFront() 
-	{
-		return $this->_contentHelperData->isCmsSyncEnabledOnFront();
-	}
-	
-	/**
+    public function isCmsSyncEnabledOnFront()
+    {
+        return $this->_contentHelperData->isCmsSyncEnabledOnFront();
+    }
+
+    /**
      * Return the escaped query text
-	 *
+     *
      * @return string
      */
-	public function getEscapedQueryText() 
-	{
-		return $this->_catalogSearchHelper->getEscapedQueryText();
-	}
-	
-	/**
+    public function getEscapedQueryText()
+    {
+        return $this->_catalogSearchHelper->getEscapedQueryText();
+    }
+
+    /**
      * Return the requested module name
-	 *
+     *
      * @return string
      */
-	public function getModuleName() 
-	{		
-		return $this->_context->getRequest()->getModuleName();
-	}
-	
-	/**
+    public function getModuleName()
+    {
+        return $this->_context->getRequest()->getModuleName();
+    }
+
+    /**
      * Check whether the content module configured
-	 *
+     *
      * @return bool
      */
-	public function isExtensionConfigured() 
-	{
-		return $this->_searchHelperConfig->isExtensionConfigured();
-	}
-	
-	/**
+    public function isExtensionConfigured()
+    {
+        return $this->_searchHelperConfig->isExtensionConfigured();
+    }
+
+    /**
      * Return the catalog search URL
-	 *
+     *
      * @return string
      */
-	public function getCatalogSearchURL()
-	{		
-		return $this->getUrl('catalogsearch/result/')."?q=".$this->getEscapedQueryText();
-	}	
-	
-	/**
+    public function getCatalogSearchURL()
+    {
+        return $this->getUrl('catalogsearch/result/') . "?q=" . $this->getEscapedQueryText();
+    }
+
+    /**
      * Return the content search URL
-	 *
+     *
      * @return string
      */
-	public function getContentSearchURL()
-	{		
-		return $this->getUrl('content/search')."?q=".$this->getEscapedQueryText();
-	}
-	
+    public function getContentSearchURL()
+    {
+        return $this->getUrl('content/search') . "?q=" . $this->getEscapedQueryText();
+    }
 }
